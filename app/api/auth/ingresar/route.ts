79812d5cf7a1e93221a36cafcdd333c748b09333
Nicolas Chirino <nicolas.brch@gmail.com>
2023-09-27 16:21:23 -0300

    Agregar mensaje de credenciales inválidas

--- app/api/auth/ingresar/route.ts
+++ app/api/auth/ingresar/route.ts
@@ -1,27 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
-import { validate } from "rut.js";
+import { validate, clean } from "rut.js";
 
 export async function POST(request: NextRequest) {
     const { rut } = await request.json()
     const rutIsValid : boolean = validate(rut)
-    if (rutIsValid){
-        // rut valido
+    if (!rutIsValid){
+        // Caso 1: Rut no cumple formato (está mal escrito o no existe)
         return NextResponse.json(
             {
-                message: rut
+                message: "Rut escrito incorrectamente. Por favor intente nuevamente."
             },
             {
-                status: 200
+                status: 500
             }
         )
-    } else {
+    } else if (clean(rut) != "204042829"){
+        // Caso 2: Credenciales inválidas (rut o contraseñas incorrectos en la bdd)
         return NextResponse.json(
             {
-                message: "Rut inválido. Por favor intente nuevamente."
+                message: "Credenciales inválidas. Por favor revise su usuario o contraseña e intente nuevamente."
             },
             {
                 status: 500
             }
         )
     }
+    return NextResponse.json(
+        {
+            message: rut
+        },
+        {
+            status: 200
+        }
+    )
 }

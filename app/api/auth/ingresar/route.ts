2542ae99448bb4dfd01bc3b6c271cd7d680c680c
Nicolas Chirino <nicolas.berrios_c@mail.udp.cl>
2023-09-26 22:20:23 -0300

    Endpoint de API el cuál valida el RUT Usando la librería rutJS se valida el rut

---
+++ app/api/auth/ingresar/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { validate } from "rut.js";
+
+export async function POST(request: NextRequest) {
+    const { rut } = await request.json()
+    const rutIsValid : boolean = validate(rut)
+    if (rutIsValid){
+        // rut valido
+        return NextResponse.json(
+            {
+                message: rut
+            },
+            {
+                status: 200
+            }
+        )
+    } else {
+        return NextResponse.json(
+            {
+                message: "Rut invalido"
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}

dadd9f46bd68956b2d3317a86cca38ab07d88c13
Nicolas Chirino <nicolas.brch@gmail.com>
2023-10-07 19:36:56 -0300

    Centrar formulario

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function SignInLayout({
     children: React.ReactNode;
     }) {
     return (
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+        <div className="relative flex flex-1 flex-col justify-center h-screen overflow-x-hidden">
             <main>
                 <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
                 {children}

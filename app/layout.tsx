f4e2660d5f0d428114650ad6de3392c03c9db1bc
Nicolas Chirino <nicolas.brch@gmail.com>
2023-10-07 18:33:59 -0300

    Modificación Layout página principal

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,11 @@ import "./satoshi.css";
 import React, { useState, useEffect } from "react";
 import Loader from "@/components/common/Loader";
 
-import Sidebar from "@/components/Sidebar-Student";
-import Header from "@/components/Header";
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -28,21 +24,7 @@ export default function RootLayout({
             <Loader />
           ) : (
             <div className="flex h-screen overflow-hidden">
-              <Sidebar
-                sidebarOpen={sidebarOpen}
-                setSidebarOpen={setSidebarOpen}   
-              />
-              <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-              <Header
-                  sidebarOpen={sidebarOpen}
-                  setSidebarOpen={setSidebarOpen}
-              />
-              <main>
-                  <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                  {children}
-                  </div>
-              </main>
-              </div>
+              {children}
             </div>
           )}
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,40 @@
+"use client"
+import React, { useState, useEffect } from "react";
+import Sidebar from "@/components/Sidebar-Student";
+import Header from "@/components/Header";
 import DashEstudiante from "@/components/Dashboard/dashEstudiante";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "LabCal",
-  description: "",
-  // other metadata
-};
 
 // pendiente
 // se debe hacer la separacion de estudiante y profesor, tendran diferentes dashboard y menus de navegacion
 
-export default function Home() {
+export default function Home({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+      setTimeout(() => setLoading(false), 1000);
+    }, []);
   return (
     <>
-      <DashEstudiante />
+      <Sidebar
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}   
+      />
+      <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+      <Header
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+      />
+      <main>
+          <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+            <DashEstudiante/>
+          </div>
+      </main>
+      </div>
     </>
   );
 }

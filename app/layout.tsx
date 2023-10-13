4a4ceb9cdf383bd047af0945babefa30d2ca9656
Nicolas Chirino <nicolas.brch@gmail.com>
2023-10-12 21:03:54 -0300

    Quitar loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,18 @@
-"use client";
 import "./globals.css";
 import "./data-tables-css.css";
 import "./satoshi.css";
-import React, { useState, useEffect } from "react";
-import Loader from "@/components/common/Loader";
+import React from "react";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
   return (
     
     <html lang="en">
       <body suppressHydrationWarning={true}>
-        <div className="dark:bg-boxdark-2 dark:text-bodydark">
-          {loading ? (
-            <Loader />
-          ) : (
-            <div className="flex h-screen overflow-hidden">
-              {children}
-            </div>
-          )}
-        </div>
+        {children}
       </body>
     </html>
   );

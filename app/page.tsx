28919314636cd615311473362c6362cc9336e7a9
Nicolas Chirino <nicolas.brch@gmail.com>
2023-10-12 21:05:43 -0300

    Agregar condicionales para verificar si existe usuario Sino existe, redirigir a /ingresar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 "use client"
+import { useRouter } from "next/navigation";
 import React, { useState, useEffect } from "react";
-import Sidebar from "@/components/Sidebar-Student";
+import SidebarStudent from "@/components/Sidebar-Student";
+import SidebarTeacher from "@/components/Sidebar-Profesor";
 import Header from "@/components/Header";
+import useLocalStorage from "@/hooks/useLocalStorage";
 import DashEstudiante from "@/components/Dashboard/dashEstudiante";
+import Loader from "@/components/common/Loader";
 
 // pendiente
 // se debe hacer la separacion de estudiante y profesor, tendran diferentes dashboard y menus de navegacion
@@ -13,28 +17,61 @@ export default function Home({
     children: React.ReactNode;
   }) {
     const [sidebarOpen, setSidebarOpen] = useState(false);
-    const [loading, setLoading] = useState<boolean>(true);
+    const [user, setUser] = useLocalStorage("user", null);
+
+    const router = useRouter();
 
     useEffect(() => {
-      setTimeout(() => setLoading(false), 1000);
-    }, []);
+        // Check if the user is not logged in, and if so, redirect to /ingresar
+        if (!user) {
+            router.push("/ingresar");
+        }
+    }, [user]);
+
   return (
     <>
-      <Sidebar
-          sidebarOpen={sidebarOpen}
-          setSidebarOpen={setSidebarOpen}   
-      />
-      <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-      <Header
-          sidebarOpen={sidebarOpen}
-          setSidebarOpen={setSidebarOpen}
-      />
-      <main>
-          <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-            <DashEstudiante/>
-          </div>
-      </main>
-      </div>
+    <div className="dark:bg-boxdark-2 dark:text-bodydark">
+        <div className="flex h-screen overflow-hidden">
+            { user !== null ? (
+                <>
+                    {user.type === 'student' ? (
+                        <SidebarStudent
+                            sidebarOpen={sidebarOpen}
+                            setSidebarOpen={setSidebarOpen}
+                        />
+                    ) : null}
+                    {user.type === 'teacher' ? (
+                        <SidebarTeacher
+                            sidebarOpen={sidebarOpen}
+                            setSidebarOpen={setSidebarOpen}
+                        />
+                    ) : null}
+                </>
+            ) : null }
+            <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                { user !== null ? (
+                    <Header
+                        sidebarOpen={sidebarOpen}
+                        setSidebarOpen={setSidebarOpen}
+                    />
+                ) : null }
+                <main>
+                    <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                        { user !== null ? (
+                            <>
+                                {user.type === 'student' ? (
+                                    <DashEstudiante/>
+                                ) : null}
+                                {user.type === 'teacher' ? (
+                                    <DashEstudiante/>
+                                ) : null}
+                            </>
+                        ) : null }
+                    </div>
+                </main>
+            </div>
+        </div>
+    </div>
     </>
   );
 }

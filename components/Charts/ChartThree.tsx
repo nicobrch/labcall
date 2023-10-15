1888e23f837fb89d44df153c85e7a1e722343bd0
Nicolas Chirino <nicolas.brch@gmail.com>
2023-10-15 19:26:40 -0300

    Agregar estadisticas del curso por eje

--- components/Charts/ChartThree.tsx
+++ components/Charts/ChartThree.tsx
@@ -2,66 +2,77 @@
 import { ApexOptions } from "apexcharts";
 import React, { useState } from "react";
 import dynamic from "next/dynamic";
+import Link from "next/link"
 const ReactApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 interface ChartThreeState {
   series: number[];
 }
 
-const options: ApexOptions = {
-  chart: {
-    type: "donut",
-  },
-  colors: ["#10B981", "#375E83", "#259AE6", "#FFA70B"],
-  labels: ["Remote", "Hybrid", "Onsite", "Leave"],
-  legend: {
-    show: true,
-    position: "bottom",
-  },
+export interface ChartThreeProps {
+  nombre: string
+  enlace: string,
+  labels: string[]; // Nuevas props para labels
+  series: number[]; // Nuevas props para series
+}
 
-  plotOptions: {
-    pie: {
-      donut: {
-        size: "65%",
-        background: "transparent",
-      },
+const ChartThree: React.FC<ChartThreeProps> = ({ enlace, nombre, labels, series }) => {
+
+  const options: ApexOptions = {
+    chart: {
+      type: "donut",
     },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  responsive: [
-    {
-      breakpoint: 2600,
-      options: {
-        chart: {
-          width: 380,
+    colors: ["#10B981", "#bf2c4e"],
+    labels: labels,
+    legend: {
+      show: true,
+      position: "bottom",
+    },
+
+    plotOptions: {
+      pie: {
+        donut: {
+          size: "65%",
+          background: "transparent",
         },
       },
     },
-    {
-      breakpoint: 640,
-      options: {
-        chart: {
-          width: 200,
+    dataLabels: {
+      enabled: false,
+    },
+    responsive: [
+      {
+        breakpoint: 2600,
+        options: {
+          chart: {
+            width: 380,
+          },
         },
       },
-    },
-  ],
-};
+      {
+        breakpoint: 640,
+        options: {
+          chart: {
+            width: 200,
+          },
+        },
+      },
+    ],
+  };
 
-const ChartThree: React.FC = () => {
   const [state, setState] = useState<ChartThreeState>({
-    series: [65, 34, 12, 56],
+    series: series,
   });
 
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-5">
       <div className="mb-3 justify-between gap-4 sm:flex">
         <div>
-          <h5 className="text-xl font-semibold text-black dark:text-white">
-            Visitors Analytics
-          </h5>
+          <Link href={enlace}>
+            <h5 className="text-xl font-semibold text-black dark:text-white">
+              {nombre}
+            </h5>
+          </Link>
         </div>
         <div>
           <div className="relative z-20 inline-block">
@@ -70,8 +81,8 @@ const ChartThree: React.FC = () => {
               id=""
               className="relative z-20 inline-flex appearance-none bg-transparent py-1 pl-3 pr-8 text-sm font-medium outline-none"
             >
-              <option value="">Monthly</option>
-              <option value="">Yearly</option>
+              <option value="">Dia</option>
+              <option value="">Semana</option>
             </select>
             <span className="absolute top-1/2 right-3 z-10 -translate-y-1/2">
               <svg
@@ -108,42 +119,16 @@ const ChartThree: React.FC = () => {
       </div>
 
       <div className="-mx-8 flex flex-wrap items-center justify-center gap-y-3">
-        <div className="w-full px-8 sm:w-1/2">
-          <div className="flex w-full items-center">
-            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-primary"></span>
-            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Desktop </span>
-              <span> 65% </span>
-            </p>
-          </div>
-        </div>
-        <div className="w-full px-8 sm:w-1/2">
-          <div className="flex w-full items-center">
-            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#6577F3]"></span>
-            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Tablet </span>
-              <span> 34% </span>
-            </p>
-          </div>
-        </div>
-        <div className="w-full px-8 sm:w-1/2">
-          <div className="flex w-full items-center">
-            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#8FD0EF]"></span>
-            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Mobile </span>
-              <span> 45% </span>
-            </p>
-          </div>
-        </div>
-        <div className="w-full px-8 sm:w-1/2">
-          <div className="flex w-full items-center">
-            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#0FADCF]"></span>
-            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Unknown </span>
-              <span> 12% </span>
-            </p>
-          </div>
-        </div>
+        {labels.map((label, index) => (
+            <div className="w-full px-8 sm:w-1/2" key={index}>
+              <div className="flex w-full items-center">
+                <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
+                  <span>{label}</span> {/* Usamos el valor del label */}
+                  <span>{series[index]}</span> {/* Usamos el valor de la serie correspondiente */}
+                </p>
+              </div>
+            </div>
+        ))}
       </div>
     </div>
   );

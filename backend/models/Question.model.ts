8f6f5408be0d68dd9e4b23cab357e0a0a23b2921
bvlebvle <valentina.diaz_go@mail.udp.cl>
2023-10-26 21:58:24 -0300

    arreglos nodos, question, axis y abilities

--- backend/models/Question.model.ts
+++ backend/models/Question.model.ts
@@ -4,44 +4,48 @@ import { IQuestion } from "../interfaces/question";
 export interface QuestionModel extends Model<IQuestion, IQuestion> {}
 
 export default function defineQuestionModel(sequelize: Sequelize) {
-  const Question = sequelize.define<QuestionModel, IQuestion>(
-    "question",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      questionText: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      imageURL: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      figureCaption: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-      },
-      node_id: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: true,
-      },
-    },
-    {
-      freezeTableName: true,
-    }
-  );
+	const Question = sequelize.define<QuestionModel, IQuestion>(
+		"question",
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true
+			},
+			questionText: {
+				type: DataTypes.TEXT,
+				allowNull: false
+			},
+			imageURL: {
+				type: DataTypes.STRING,
+				allowNull: true
+			},
+			figureCaption: {
+				type: DataTypes.STRING(255),
+				allowNull: true
+			},
+			node_id: {
+				type: DataTypes.INTEGER,
+				allowNull: true
+			},
+			dificulty: {
+				type: DataTypes.STRING(20),
+				allowNull: true
+			},
+			createdAt: {
+				type: DataTypes.DATE,
+				allowNull: false,
+				defaultValue: DataTypes.NOW
+			},
+			updatedAt: {
+				type: DataTypes.DATE,
+				allowNull: true
+			}
+		},
+		{
+			freezeTableName: true
+		}
+	);
 
-  return Question;
+	return Question;
 }
--- pages/api/axis/index.ts
+++ pages/api/axis/index.ts
@@ -4,6 +4,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	try {
 		const axis = await getAllAxis();
+		console.log(axis);
 		res.status(200).send(axis);
 	} catch (error: any) {
 		res.status(500).send({

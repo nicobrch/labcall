02266065d9e83601380caefdcb46de163c6e3684
Nicolas Chirino <nicolas.brch@gmail.com>
2023-10-29 01:03:13 -0300

    Cargar estudiantes usando archivo Excel done

--- pages/api/uploadExcel/index.ts
+++ pages/api/uploadExcel/index.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import formidable from 'formidable';
 import * as XLSX from 'xlsx';
+import { validate, clean } from "rut.js";
+import { addStudent } from "@/backend/services/user.service";
 
 export const config = {
   api: {
@@ -9,11 +11,11 @@ export const config = {
 };
 
 export interface rowData {
-  rut: string,
-  nombres: string,
-  apellido_paterno: string,
-  apellido_materno: string,
-  email: string
+  rut?: string | number | null | undefined,
+  nombres?: string | null | undefined,
+  primer_apellido?: string | null | undefined,
+  segundo_apellido?: string | null | undefined,
+  email?: string | null | undefined,
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -21,44 +23,77 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
       const form = formidable({});
       
-      form.parse(req, (err, fields, files) => {
+      form.parse(req, async (err, fields, files) => {
         if (err) {
-          res.status(500).json({ error: 'Server error.' });
+          res.status(400).json({error: 'Server error.'});
           return;
         }
-        if (!files.excelFile){
-          res.status(400).json({ error: 'No file uploaded.' })
+        if (!files.excelFile) {
+          res.status(400).json({error: 'No file uploaded.'})
+          return;
+        }
+        if (!fields.courseId) {
+          res.status(400).json({error: 'No course selected.'})
           return;
         }
         const file = files.excelFile[0];
-        if (!file.originalFilename?.endsWith('.xlsx')){
-          res.status(400).json({ error: 'The file is not XLSX.' })
+        const courseId = fields.courseId[0];
+        
+        if (!file.originalFilename?.endsWith('.xlsx')) {
+          res.status(400).json({error: 'The file is not XLSX.'})
           return;
         }
         // El archivo existe y es de tipo XLSX, procesar
         const workbook = XLSX.readFile(file.filepath);
         const firstSheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[firstSheetName];
-        // Convert the sheet data to JSON
-        const data = XLSX.utils.sheet_to_json<rowData>(worksheet);
-        // Verify that the data has all the columns specified in the SheetData interface
-        const requiredColumns = rowData.keys();
-        
-        for (const row of data) {
-          for (const col of requiredColumns) {
-            if (!(col in row)) {
-              res.status(400).json({ error: `Missing column '${col}' in the Excel data.` });
-              return;
-            }
+        // Definir filas y columnas como JSON
+        const excelData = XLSX.utils.sheet_to_json<rowData>(worksheet);
+        // Procesar JSON
+        let studentsInsertedCount: number = 0;
+        for (let index = 0; index < excelData.length; index++) {
+          const row = excelData[index];
+          // Saltar filas con valores nulos
+          if (!row.rut || !row.nombres || !row.primer_apellido || !row.segundo_apellido || !row.email) {
+            continue;
           }
+          row.rut = String(row.rut);
+          // Saltar filas con ruts invalidos
+          if (!validate(row.rut)) {
+            continue;
+          }
+          row.rut = clean(row.rut)
+          // Constantes para crear usuario
+          const password = row.rut.substring(0, 4)
+          const type = "student"
+          const active = true
+          // Insertar estudiantes
+          try {
+            await addStudent(
+              row.rut,
+              row.nombres.toLowerCase(),
+              row.primer_apellido.toLowerCase(),
+              row.segundo_apellido.toLowerCase(),
+              row.email,
+              password,
+              type,
+              active,
+              parseInt(courseId),
+            )
+            studentsInsertedCount++;
+          } catch (error) {
+            console.error(error);
+            res.status(500).json({error: `Insert error: ${error}`});
+          }
+          
+          res.status(200).json({message: `Se insertaron exitosamente ${studentsInsertedCount} estudiantes.`});
         }
-        res.status(200).json({data: data});
       });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Server error.' });
+      res.status(500).json({ error: `Server error. ${error}` });
     }
   } else {
-    res.status(405).end(); // Method not allowed.
+    res.status(405).end(); // Method not allowed
   }
 }
